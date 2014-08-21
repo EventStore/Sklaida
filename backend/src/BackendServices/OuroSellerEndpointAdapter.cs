31ae907a5a938a1e43a72ddfcb61306734c608e0
Greg Young <gregoryyoung1@gmail.com>
2014-08-21 16:50:30 -0700

    add json methods and ouroendpoint adapter

--- backend/src/BackendServices/OuroSellerEndpointAdapter.cs
+++ backend/src/BackendServices/OuroSellerEndpointAdapter.cs
@@ -1,23 +1,28 @@
 ﻿using System;
+using System.Text;
 using EventStore.ClientAPI;
 using EventStore.ClientAPI.SystemData;
+using Messages;
 
 namespace BackendServices
 {
     public class OuroSellerEndpointAdapter
     {
         private readonly IEventStoreConnection _connection;
+        private readonly string _adapterName;
         private readonly IOuroSellerEndpoint _endpoint;
         private readonly string _incomingStream;
         private readonly UserCredentials _credentials;
         private EventStoreSubscription _subscription;
 
         public OuroSellerEndpointAdapter(IEventStoreConnection connection, 
+                                         string adapterName,
                                          IOuroSellerEndpoint endpoint, 
                                          string incomingStream, 
                                          UserCredentials credentials=null)
         {
             _connection = connection;
+            _adapterName = adapterName;
             _endpoint = endpoint;
             _incomingStream = incomingStream;
             _credentials = credentials;
@@ -27,19 +32,39 @@ namespace BackendServices
         {
             _subscription = _connection.SubscribeToStreamAsync(_incomingStream, 
                                                                false, 
-                                                               eventAppeared,
-                                                               subscriptionDropped, 
+                                                               EventAppeared,
+                                                               SubscriptionDropped, 
                                                                _credentials).Result;
         }
 
-        private void subscriptionDropped(EventStoreSubscription arg1, SubscriptionDropReason arg2, Exception arg3)
+        public void Stop()
         {
-            throw new NotImplementedException();
+            _subscription.Close();
+            _subscription.Dispose();
         }
 
-        private void eventAppeared(EventStoreSubscription arg1, ResolvedEvent arg2)
+        private void SubscriptionDropped(EventStoreSubscription sub, SubscriptionDropReason reason, Exception exception)
         {
-            throw new System.NotImplementedException();
+            Log.Debug(_adapterName + " lost subscription due to " + reason);
+        }
+
+        private void EventAppeared(EventStoreSubscription sub, ResolvedEvent e)
+        {
+            Log.Debug(_adapterName + " received " + Encoding.UTF8.GetString(e.Event.Data));
+            SearchRequested request = null;
+            try
+            {
+                var response = _endpoint.GetQuoteFor(request);
+                Publish(request.ClientResponseStream, response);
+            }
+            catch (Exception ex)
+            {
+                Log.Exception(_adapterName + " endpoint caused exception", ex);
+            }
+        }
+
+        private void Publish(string clientResponseStream, Event response)
+        {
         }
     }
 }
